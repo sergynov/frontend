4d4577d2efa9aa668a37305bddd7e003bc31aa40
sergynov <sergeynov07@gmail.com>
2025-09-22 21:52:14 +0200

    Add files via upload

---
+++ 2.9-2/src/components/editTask.jsx
@@ -0,0 +1,51 @@
+import { useDispatch, useSelector } from 'react-redux'
+import styles from '../app.module.css'
+import { selectIsModalOpen } from '../seletors/selectModal'
+import { selectNewTitle } from '../seletors/selectNewTitle'
+import { selectNewBody } from '../seletors/selectNewBody'
+import { requestChangeTask } from '../service/taskService'
+
+
+export const EditTask = () => {
+
+
+  const dispatch = useDispatch()
+  const isModalOpen = useSelector(selectIsModalOpen)
+  const newTitle = useSelector(selectNewTitle)
+  const newBody = useSelector(selectNewBody)
+  const currentTask = useSelector(state=>state.form.currentTask)
+
+  const handleSave = () => {
+    if (!currentTask) return;
+    dispatch(requestChangeTask(currentTask.id,newTitle,newBody))
+  }
+
+  return(
+    <>
+    <div>
+              {isModalOpen && (
+            <div className={styles.modalOverlay}>
+              <div className={styles.modal}>
+                <h2>Edit Task</h2>
+                <input
+                  type="text"
+                  value={newTitle}
+                  onChange={(e) => dispatch({type:'FORM/SET_TITLE', payload:e.target.value})}
+                  placeholder="Title"
+                />
+                <textarea
+                  value={newBody}
+                  onChange={(e) => dispatch({type:'FORM/SET_BODY', payload:e.target.value})}
+                  placeholder="Body"
+                />
+                <div className={styles.modalButtons}>
+                  <button onClick={()=>handleSave(currentTask.id,newTitle,newBody)}>Save</button>
+                  <button onClick={() => dispatch({type:'DISPLAY/TOGGLE_MODAL',payload:false})}>Cancel</button>
+                </div>
+              </div>
+            </div>
+            )}
+            </div>
+    </>
+  )
+}
---
+++ 2.9-2/src/components/header.jsx
@@ -0,0 +1,19 @@
+import styles from '../app.module.css'
+import { useDispatch, useSelector } from 'react-redux'
+import {selectSearchTask} from '../seletors/selectSearchTask'
+import { selectIsSorted } from '../seletors/selectIsSorted'
+
+export const Header = () => {
+
+  const isSorted = useSelector(selectIsSorted)
+  const searchTask = useSelector(selectSearchTask)
+  const dispatch = useDispatch()
+
+  return(
+    <div>
+      <input type="text" placeholder='Поиск' value={searchTask} onChange={(e)=> dispatch({type:'DISPLAY/SET_SEARCH',payload:e.target.value})} className={styles.search} />
+      <button className={styles.button} onClick={()=>dispatch({type:'DISPLAY/TOGGLE_SORT'})}>
+        {isSorted ? 'Выключить сортировку' : 'Сортировать по алфавиту'}</button>
+    </div>
+  )
+}
