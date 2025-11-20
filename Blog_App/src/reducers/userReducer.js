da340b8287e42c9ae9e223e103c621f9cd3f8d62
sergynov <sergeynov07@gmail.com>
2025-11-20 21:42:17 +0100

    Add files via upload

---
+++ Blog_App/src/reducers/userReducer.js
@@ -0,0 +1,28 @@
+import { ROLE } from "../../constants/role"
+import { ACTION_TYPE } from "../actions/action-types"
+
+const initialUserState = {
+  id: null,
+  login: null,
+  roleId: ROLE.GUEST,
+  session: null
+}
+
+export const userReducer = (state=initialUserState,action) => {
+  switch (action.type) {
+
+    case ACTION_TYPE.SET_USER: {
+      return {
+        ...state,
+        ...action.payload
+      }
+    }
+
+    case ACTION_TYPE.LOGOUT:{
+      return initialUserState;
+    }
+
+    default: 
+      return state
+  }
+}
