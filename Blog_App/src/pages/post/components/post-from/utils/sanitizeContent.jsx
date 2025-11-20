bb0cb8ccccc81a9b87eeb2c2083ba09a01a94112
sergynov <sergeynov07@gmail.com>
2025-11-20 21:39:43 +0100

    Delete Blog_App/src/pages/post/components/post-from directory

--- Blog_App/src/pages/post/components/post-from/utils/sanitizeContent.jsx
+++
@@ -1,6 +0,0 @@
-export const sanitizeContent = (content) => content
-  .replaceAll('&nbsp;',' ')
-  .replace(/ +/, ' ')
-  .replaceAll('<div><br></div>', '\n')
-  .replaceAll('<div>', '\n')
-  .replaceAll('</div>', '')
